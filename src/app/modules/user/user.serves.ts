efb858182d99fc020e3533697584b8344ec4d3c2
Manik <tonysins43@gmail.com>
2023-11-23 00:40:23 +0600

    make interface for user

---
+++ src/app/modules/user/user.serves.ts
@@ -0,0 +1,23 @@
+export interface TUser {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: {
+    firstName: string;
+    lastName: string;
+  };
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: string[];
+  address: {
+    street: string;
+    city: string;
+    country: string;
+  };
+  orders?: {
+    productName: string;
+    price: number;
+    quantity: number;
+  }[];
+}
